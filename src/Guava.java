fbbe9d6bb4bb02fb6c19fd15efdb359199821d78
Rory Preddy <rorypreddy@gmail.com>
2023-07-10 12:43:55 +0200

    Guava

---
+++ src/Guava.java
@@ -0,0 +1,9 @@
+import com.google.common.collect.ImmutableList;
+
+public class Guava {
+
+    public static void main(String[] args) {
+        ImmutableList<String> names = ImmutableList.of("Alice", "Bob", "Charlie");
+        System.out.println(names);
+    }
+}
