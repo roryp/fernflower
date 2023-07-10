88e14fea42a41b879ea794fb6050ffaccfcab09d
Rory Preddy <rorypreddy@gmail.com>
2023-07-10 12:21:39 +0200

    gson

--- src/App.java
+++ src/App.java
@@ -1,16 +1,18 @@
-import org.apache.commons.lang3.StringUtils;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+
 
 public class App {
     public static void main(String[] args) {
-        // Check if a string is empty or null
-        String str = null;
-        boolean isEmpty = StringUtils.isEmpty(str);
-        
-        // Capitalize the first letter of a string
-        String str2 = "hello world";
-        String capitalized = StringUtils.capitalize(str2);        
 
-        System.out.println("Is the string empty? " + isEmpty);
-        System.out.println("Capitalized string: " + capitalized);
+        List<String> list = new ArrayList<String>();
+        list.add("name");
+        Gson gson = new Gson();
+        String json1 = gson.toJson(list);
+        String json2 = gson.toJson(list,ArrayList.class);
+        System.out.println("json serialization of list: " + json2);
+        
     }
 }
